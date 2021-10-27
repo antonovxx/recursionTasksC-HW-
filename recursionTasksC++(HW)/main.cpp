6086f20061fc97b96294d8425620b617a44094e0
antonkurin <antonkurin1@gmail.com>
2021-10-27 14:18:35 +0300

    Initial Commit

---
+++ recursionTasksC++(HW)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  recursionTasksC++(HW)
+//
+//  Created by Антон Курин on 27.10.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
